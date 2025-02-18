9bfec916c1b86dc1dcd7864a3f1d98ecf88f2798
CesarSM <chechare71@gmail.com>
2025-02-18 15:19:46 +0100

    Agregar archivo 01-Variables.js con ejemplos de var, let y const en JavaScript

---
+++ 01-Variables.js
@@ -0,0 +1,21 @@
+// var
+
+var helloWorld = "Hello JavaScript"
+console.log(helloWorld)
+var helloWorld = "Hello JavaScript 2"   
+console.log(helloWorld)
+
+
+// let (ES6, Son las que se deben usar)
+
+let helloWorld2 = "Hello JavaScript"
+console.log(helloWorld2)
+helloWorld2 = "Hello JavaScript 2"  
+console.log(helloWorld2)
+
+//const (ES6, Son las que se deben usar)
+
+const helloWorld3 = "Hello JavaScript 3"    
+console.log(helloWorld3)
+const Pi= 3.1416
+console.log(Pi)
