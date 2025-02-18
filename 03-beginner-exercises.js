e30d888e2e736fe4f58145e096c518210676b285
CesarSM <chechare71@gmail.com>
2025-02-19 00:45:01 +0100

    Create 03-beginner-exercises.js

---
+++ 03-beginner-exercises.js
@@ -0,0 +1,46 @@
+// 1. Escribe un comentario en una línea.
+// hola esto es un comentario simple.
+// 2. Escribe un comentario en varias líneas.
+/* Hola esto es un comeentario 
+compuesto
+de varias lineas */
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos.
+let nombre = "César";
+let alias = 'Cesir72';
+let edad = 52;
+// 4. Imprime por consola el valor de todas las variables.
+console.log(nombre)
+console.log(alias)
+let saludo = `Hola ${nombre}`
+console.log(saludo)
+
+console.log("-----------------------------")
+
+// 5. Imprime por consola el tipo de todas las variables.
+console.log(typeof nombre)
+console.log(typeof edad)
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo.
+let nombreNuevo = "César Antonio"
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo.
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos.
+    const nombreConst = "César"
+    const constString = "Soy una constante"
+    const constNumber = 52
+    const constBoolean = false
+    const constNull = null
+    const constUndefined = undefined
+    const constBigInt = BigInt(789654123)
+    const constSymbol = Symbol("constantSymbol")
+// 9. A continuación, modifica los valores de las constantes.
+// const nombreConst = "César"
+// const constString = "Soy una constante"
+// const constNumber = 52
+// const constBoolean = false
+// const constNull = null
+// const constUndefined = undefined
+// const constBigInt = BigInt(789654123)
+// const constSymbol = Symbol("constantSymbol")
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse.
